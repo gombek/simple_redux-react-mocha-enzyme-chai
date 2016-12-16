8ac3c8c219ae2735b92d25a7487b6acaf2a6cb87
Kuba <kuba.piechowicz@joivy.com>
2016-12-16 11:00:59 +0100

    React is added.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('My SoundCloud React Redux App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>Title goes here</div>,
+    document.getElementById('app')
+);
